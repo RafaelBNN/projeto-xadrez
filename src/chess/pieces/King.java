9bc139c9d2498eb29c16d951bf9bf2efe4d779ed
Rafael <rafaelbnneto@gmail.com>
2022-08-03 07:37:41 -0300

    Implementing possible king moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
@@ -21,9 +22,49 @@ public class King extends ChessPiece {
         this.chessMatch = chessMatch;
     }
 
+    private boolean canMove(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return (p==null || p.getColor()!=this.getColor());
+    }
+
     @Override
     public boolean[][] possibleMoves(){
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // testando movimento para cima
+        p.setValues(position.getRow()-1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para baixo
+        p.setValues(position.getRow()+1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a direita
+        p.setValues(position.getRow(), position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a esquerda
+        p.setValues(position.getRow(), position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a diagonal cima-esquera
+        p.setValues(position.getRow()-1, position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a diagonal cima-direita
+        p.setValues(position.getRow()-1, position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a diagonal baixo-esquerda
+        p.setValues(position.getRow()+1, position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
+        // testando movimento para a diagonal baixo-direita
+        p.setValues(position.getRow()+1, position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+
         return mat;
     }
 
