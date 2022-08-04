65136f6728efc1af5ac24955c104a2bfefe01d72
Rafael <rafaelbnneto@gmail.com>
2022-08-04 15:22:18 -0300

    Updating promotion logic

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,11 @@ public class Main {
 
                 if(chessMatch.getPromoted()!=null){
                     System.out.print("Enter piece for promotion (Q/R/B/N): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.print("Invalid value. Enter piece for promotion (Q/R/B/N): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             }
